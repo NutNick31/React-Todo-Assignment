ad55ecdf106b9ec64c806e0b403301e4ec0b52fb
Raj <rajesh211019study@gmail.com>
2022-08-30 18:50:04 +0530

    First Commit

---
+++ react-todo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoForm from './components/todoForm';
+import ToDoList from './components/todoList';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
